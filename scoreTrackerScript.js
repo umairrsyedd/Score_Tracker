0b9a884b53b5c43ae329388b6b4d4e01b02919e5
Syed Umair <umairrsyedd@gmail.com>
2020-01-19 04:49:11 +0530

    All Css completed except alligning of the reset button

---
+++ scoreTrackerScript.js
@@ -0,0 +1,13 @@
+// Get the input entered in the number of rounds
+
+// Set the input to the max score
+
+// if player 1 button is clicked increment score p1
+
+// if player 2 button is clicked increment score of p2
+
+// If any player wins the game the display the winning player 
+// with a winning button symbol
+
+// if reset button is clicked then reset everything including the 
+// player score and the max score 
