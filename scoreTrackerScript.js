eb8b672eaba59e4d890ed8f67c3bdb023676ec18
Syed Umair <umairrsyedd@gmail.com>
2020-01-19 21:14:42 +0530

    Project Conpleted !

--- scoreTrackerScript.js
+++ scoreTrackerScript.js
@@ -16,15 +16,14 @@ function editMaxScore(){
 // if player 2 button is clicked increment score of p2
 let p1=0;
 let p2=0;
+var game=true;
 document.querySelector("#p1-in").addEventListener("click",function(){
-    if(p1==score){
-    console.log("Player 1 Has Won The Game")
-    }
-    else{
         p1=p1+1;
-    }
+   
     player1display();
-    
+    if(p1==score){
+        winningmessage("Player 1  ");
+}
 })
 document.querySelector("#p1-de").addEventListener("click",function(){
     if(p1<0 || p1==0) {
@@ -37,14 +36,14 @@ document.querySelector("#p1-de").addEventListener("click",function(){
     
 })
 document.querySelector("#p2-in").addEventListener("click",function(){
+   
+    p2=p2+1;
+    
+    player2display();
     if(p2==score)
     {
         winningmessage("Player 2  ");
     }
-    else{
-    p2=p2+1;
-    }
-    player2display()
 })
 document.querySelector("#p2-de").addEventListener("click",function(){
     if(p2<0 || p2==0)
@@ -58,19 +57,26 @@ document.querySelector("#p2-de").addEventListener("click",function(){
 })
 
 function player1display(){
+    if(game==true){
 let p1score=document.querySelector(".player1");
 p1score.textContent= "Score : " + p1;
+    }
 }
 function player2display(){
+    if(game==true){
 let p2score=document.querySelector(".player2");
 p2score.textContent= "Score : " + p2;
+    }
 }
 
 // If any player wins the game the display the winning player 
 // with a winning button symbol
 function winningmessage(player){
     let victory= document.querySelector("#D-score");
+    if(game==true){
     victory.textContent=player + "Won"
+    game=false;
+    }
 }
 // if reset button is clicked then reset everything including the 
 // player score and the max score 
